13d4f3ddd2d4ac30387be90ddd7d92f58e0f7038
bensoer <ikben@shaw.ca>
2015-03-23 13:45:23 -0700

    Initial Commit

---
+++ ios-timetraveler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-timetraveler
+//
+//  Created by Ben Soer on 2015-03-23.
+//  Copyright (c) 2015 bensoer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
